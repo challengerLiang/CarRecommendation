034053988b1430aabffb930fad71d6851d0f1aec
dreamerLiang <506425878@qq.com>
2018-12-03 13:50:27 -0500

    data preprocessing

--- carRecommendation.py
+++ carRecommendation.py
@@ -15,15 +15,41 @@ def readCsvFile(file):
     return listOfRawText
 
 
-def getFeaturesFromRawText(listOfRawText):
+def getFeaturesDictFromRawText(listOfRawText):
+    wholeText = []
     for i in range(1, len(listOfRawText)):
-        #print (listOfRawText[0])
-        price = listOfRawText[i][15]
-        print (price)
-        return price
+        print (listOfRawText[0])
+        sentence = ""
+        for j in range(len(listOfRawText[i])):
+            if j == 2:
+                sentence += 'year of ' + listOfRawText[i][j] + ' '
+            elif j == 4:
+                sentence += 'Engine HP ' + listOfRawText[i][j] + ' '
+            elif j == 5:
+                sentence += 'Engine Cylinders ' + listOfRawText[i][j] + ' '
+            elif j == 8:
+                sentence += 'Number of Doors ' + listOfRawText[i][j] + ' '
+            elif j == 12:
+                sentence += 'highway MPG ' + listOfRawText[i][j] + ' '
+            elif j == 13:
+                sentence += 'city mpg' + listOfRawText[i][j] + ' '
+            elif j == 14:
+                continue
+            elif j == 15:
+                sentence += 'price ' + listOfRawText[i][j]
+            else:
+                sentence += listOfRawText[i][j] + ' '
+        wholeText.append(sentence)
 
+    [print(item) for item in wholeText]
+    return wholeText
+
+def inferenceExactMatch(data,parameters):
+    for j in len(parameters):
+        if parameters[j] != '-1':
+            parameter =  parameters[j]
+            resList = []
 
 
 listOfRawText = readCsvFile(sys.argv[1])
-price = getFeaturesFromRawText(listOfRawText)
-print(price)
+price = getFeaturesDictFromRawText(listOfRawText)
