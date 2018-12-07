c6ae0c6d66d6a2c2df34d8744fc0621d13a56358
YangShi <yangshi0302@gmail.com>
2018-12-07 00:19:03 -0500

    modified pre1

--- classifier.py
+++ classifier.py
@@ -6,13 +6,13 @@ import numpy as np
 
 
 def read_csv(file):
-    listOfRawText = []
+    raw_list = []
     with open(file, 'r') as csvFile:
-        csvReader = csv.reader(csvFile)
-        for row in csvReader:
-            listOfRawText.append(row)
+        csv_reader = csv.reader(csvFile)
+        for row in csv_reader:
+            raw_list.append(row)
     csvFile.close()
-    return listOfRawText[1:]
+    return raw_list[1:]
 
 
 def random_split(listOfRawText):
@@ -25,15 +25,48 @@ def random_split(listOfRawText):
         dev_list.append(listOfRawText[index[i]])
     return train_list, dev_list
 
-def feature_selection(raw_data):
-    data = []
-    for item in raw_data:
-        data.append([item[0].lower(), item[1].lower(), item[2], item[4], item[5],
-                     item[6].lower(), item[7].split(" ")[0].lower(), item[12], item[13]])
-    return data
 
-list_raw = read_csv("data.csv")
-train_data, dev_data = random_split(list_raw)
-dev_set = feature_selection(dev_data)
-print(dev_set)
+def feature_selection(train_dev_data, test_data):
+    train_dev = []
+    test = []
+    for item in train_dev_data:
+        train_dev.append([item[0].lower(), item[1].lower(), item[2], item[4], item[5],
+                          item[6].lower(), item[7].split(" ")[0].lower(), item[12], item[13]])
+    for item in test_data:
+        test.append([item[11].lower(), item[9].split(" ")[2].lower(), item[17], item[7], item[3].split(" ")[2],
+                     item[1].split(" ")[0].lower(), item[2].split(" ")[0].split("-")[0].lower(), item[6], item[0]])
+    return train_dev, test
+
+
+def feature_revise(raw_feature):
+    make = []
+    model = []
+    transmission = []
+    drive = []
+    print(raw_feature)
+    for item in raw_feature:
+        if item[0] not in make:
+            make.append(item[0])
+        if item[1] not in model:
+            model.append(item[1])
+        if item[5] not in transmission:
+            transmission.append(item[5])
+        if item[6] not in drive:
+            drive.append(item[6])
+    print(make)
+    print(model)
+    print(transmission)
+    print(drive)
+    print(drive.index("four"))
+
+
+list_raw_train_dev = read_csv("data.csv")
+list_raw_test = read_csv("cars.csv")
+raw_feature_train_dev, raw_feature_test = feature_selection(list_raw_train_dev, list_raw_test)
+print([item for item in raw_feature_train_dev][0:100])
+print([item for item in raw_feature_test][0:100])
+# train_data, dev_data = random_split(list_raw)
+# dev_set = feature_selection(dev_data)
+# train_set = feature_selection(train_data)
+# dev_rev = feature_revise(dev_set)
 
