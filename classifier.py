3472ede02bcefdb73bc119e6c9093fa8d109e98b
YangShi <yangshi0302@gmail.com>
2018-12-07 17:36:41 -0500

    cla2

--- classifier.py
+++ classifier.py
@@ -26,14 +26,14 @@ def read_csv(file):
     return raw_list[1:]
 
 
-def random_split(listOfRawText):
-    index = np.random.permutation(len(listOfRawText))
+def random_split(data):
+    index = np.random.permutation(len(data))
     train_list = []
     dev_list = []
     for i in range(0, int(len(index) * 0.8)):
-        train_list.append(listOfRawText[index[i]])
+        train_list.append(data[index[i]])
     for i in range(int(len(index) * 0.8), len(index)):
-        dev_list.append(listOfRawText[index[i]])
+        dev_list.append(data[index[i]])
     return train_list, dev_list
 
 
@@ -42,7 +42,7 @@ def feature_selection(train_dev_data, test_data):
     test = []
     for item in train_dev_data:
         train_dev.append([item[0].lower(), item[1].lower(), item[4], item[5],
-                          item[6].lower(), item[7].split(" ")[0].lower(), item[12], item[13]])
+                          item[6].lower(), item[7].split(" ")[0].lower(), item[12], item[13], item[-1]])
     for item in test_data:
         test.append([item[11].lower(), item[9].split(" ")[2].lower(), item[7], item[3].split(" ")[2],
                      item[1].split(" ")[0].lower(), item[2].split(" ")[0].split("-")[0].lower(), item[6], item[0]])
@@ -51,7 +51,7 @@ def feature_selection(train_dev_data, test_data):
 
 def feature_revise(raw_train_dev, raw_test):
     revise_train_dev = []
-    print(raw_train_dev[350])
+    revise_test = []
     raw_total = raw_train_dev + raw_test
     make = []
     model = []
@@ -111,9 +111,81 @@ def feature_revise(raw_train_dev, raw_test):
         vec_3 = -1
         if items[3] in cylinders:
             vec_3 = int(items[3])
+
+        gold = -1
+        price = int(items[-1])
+        if price < 10000:
+            gold = 0
+        elif price >= 10000 and price < 20000:
+            gold = 1
+        elif price >= 20000 and price < 30000:
+            gold = 2
+        elif price >= 30000 and price < 40000:
+            gold = 3
+        elif price >= 40000 and price < 50000:
+            gold = 4
+        elif price >= 50000 and price < 60000:
+            gold = 5
+        elif price >= 60000 and price < 70000:
+            gold = 6
+        elif price >= 70000 and price < 100000:
+            gold = 7
+        elif price >= 100000 and price < 200000:
+            gold = 8
+        else:
+            gold = 9
+
         revise_train_dev.append([make.index(items[0]), model.index(items[1]), vec_2, vec_3,
-                                 transmission.index(items[4]), drive.index(items[5]), vec_6, vec_7])
-    print(revise_train_dev[350])
+                                 transmission.index(items[4]), drive.index(items[5]), vec_6, vec_7, gold])
+
+    for items in raw_test:
+        hp = -1
+        if not items[2] == '':
+            hp = int(items[2])
+        else:
+            hp = 400
+        vec_2 = -1
+        if hp < 150:
+            vec_2 = 0
+        elif hp >= 150 and hp < 250:
+            vec_2 = 1
+        elif hp >= 250 and hp < 350:
+            vec_2 = 2
+        elif hp >= 350 and hp < 450:
+            vec_2 = 3
+        elif hp >= 450 and hp < 550:
+            vec_2 = 4
+        else:
+            vec_2 = 5
+        hw_mpg = int(items[6])
+        ct_mpg = int(items[7])
+        vec_6 = -1
+        vec_7 = -1
+        if hw_mpg < 17:
+            vec_6 = 0
+        elif hw_mpg >= 17 and hw_mpg < 23:
+            vec_6 = 1
+        elif hw_mpg >= 23 and hw_mpg < 27:
+            vec_6 = 2
+        else:
+            vec_6 = 3
+        if ct_mpg < 12:
+            vec_7 = 0
+        elif ct_mpg >= 12 and hw_mpg < 18:
+            vec_7 = 1
+        elif ct_mpg >= 18 and hw_mpg < 22:
+            vec_7 = 2
+        else:
+            vec_7 = 3
+
+        vec_3 = -1
+        if items[3] in cylinders:
+            vec_3 = int(items[3])
+
+        revise_test.append([make.index(items[0]), model.index(items[1]), vec_2, vec_3,
+                            transmission.index(items[4]), drive.index(items[5]), vec_6, vec_7])
+
+    return revise_train_dev, revise_test
 
 
 def feature_check(raw_train_dev, raw_test):
@@ -163,7 +235,10 @@ list_raw_train_dev = read_csv("data.csv")
 list_raw_test = read_csv("cars.csv")
 raw_feature_train_dev, raw_feature_test = feature_selection(list_raw_train_dev, list_raw_test)
 # feature_check(raw_feature_train_dev, raw_feature_test)
-feature_revise(raw_feature_train_dev, raw_feature_test)
+train_dev, test = feature_revise(raw_feature_train_dev, raw_feature_test)
+train, dev = random_split(train_dev)
+print(dev)
+
 
 # simply tf-idf-vectorizer:
 
