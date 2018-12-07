113c80583a1902eb31061a6e783bcb29773cba6a
YangShi <yangshi0302@gmail.com>
2018-12-07 08:09:14 -0500

    update fe

--- carRecommendation.py
+++ carRecommendation.py
@@ -27,13 +27,13 @@ def getFeaturesDictFromRawText(listOfRawText):
         sentence = ""
         for j in range(len(listOfRawText[i])):
             if j == 2:
-                sentence += 'year of ' + listOfRawText[i][j] + ' '
+                sentence += 'year' + listOfRawText[i][j] + ' '
             elif j == 4:
                 sentence += 'Engine HP ' + listOfRawText[i][j] + ' '
             elif j == 5:
                 sentence += 'Engine Cylinders ' + listOfRawText[i][j] + ' '
             elif j == 8:
-                sentence += 'Number of Doors ' + listOfRawText[i][j] + ' '
+                sentence += 'Doors Number ' + listOfRawText[i][j] + ' '
             elif j == 12:
                 sentence += 'highway MPG ' + listOfRawText[i][j] + ' '
             elif j == 13:
